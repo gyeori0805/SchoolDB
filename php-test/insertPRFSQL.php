8a06b23c71c13dafbe1a2e20447192a253384ede
김겨리-SMU <81796346+gyeori0805@users.noreply.github.com>
2024-05-27 15:36:45 +0900

    Add files via upload

---
+++ php-test/insertPRFSQL.php
@@ -0,0 +1,40 @@
+<?php header('Content-Type: text/html; charset=UTF-8'); ?>
+<?php 
+	// 이전 페이지에서 전달 받은 메시지 확인
+	$prof_id = $_POST['prof_id'];
+	$prof_name = $_POST['prof_name'];
+	$prof_lab = $_POST['prof_lab'];
+	$prof_phonenumber = $_POST['prof_phonenumber'];
+	$major = $_POST['major'];
+	$message = "";
+
+	// MySQL 드라이버 연결 
+	include("./SQLconstants.php");
+	$conn = mysqli_connect( $mySQL_host, $mySQL_id, $mySQL_password, $mySQL_database ) or die( "Can't access DB" );
+
+	// MySQL 책 추가 실행 	
+	$query = "INSERT INTO prof( prof_id, prof_name, prof_lab, prof_phonenumber, major ) VALUES ( '".$prof_id."', '".$prof_name."', '".$prof_lab."', '".$prof_phonenumber."', '".$major."');"; 
+	$result = mysqli_query( $conn, $query );
+	if( !$result ) 
+	{	
+		$message = "(".$prof_name.") 교수를 추가하였습니다."; 
+	} 
+	else 
+	{
+		$message = "명단에 추가할 수 없습니다."; 
+	} 
+?>
+
+<?php 
+	// 로그 데이터 추출
+	include("./log.php");
+	writeLog( $message );
+?>
+
+<!-- 다음 페이지로 메시지 전달 -->
+<form name = "frm" method = "post" action = "./searchPRF.php" >
+	<input type = 'hidden' name = 'message' value = ' * <?php echo $message;?>' >
+</form>
+<script language="javascript">
+	document.frm.submit();
+</script>
