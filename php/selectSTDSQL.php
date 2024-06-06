2eb4c85f9026acb262c96997e929c20640ac7577
김겨리-SMU <81796346+gyeori0805@users.noreply.github.com>
2024-06-06 23:04:00 +0900

    Update selectSTDSQL.php

--- php/selectSTDSQL.php
+++ php/selectSTDSQL.php
@@ -1,11 +1,13 @@
 <?php header('Content-Type: text/html; charset=UTF-8'); ?>
 <?php 
-	echo "<html>";
-	echo "<table>";
-	echo "<tr>";
-	echo "<td>학년</td>";
-	echo "<td>학번</td>";
-	echo "</tr>";
-	echo "</table>";
-	echo "</html>";
+	echo "
+ 	<html>
+	<table>
+	<tr>
+	<td>학년</td>
+	<td>학번</td>
+	</tr>
+	</table>
+	</html>
+ 	";
 ?>
