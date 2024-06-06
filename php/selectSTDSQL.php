7d5abb3bb92bae9bc811a065bd5c1dce03519a8c
김겨리-SMU <81796346+gyeori0805@users.noreply.github.com>
2024-06-06 22:53:13 +0900

    Update selectSTDSQL.php

--- php/selectSTDSQL.php
+++ php/selectSTDSQL.php
@@ -1,6 +1,6 @@
 <?php header('Content-Type: text/html; charset=UTF-8'); ?>
 <?php 
-	$xzy = <<<HTML
+
 	<table border="1" bordercolor="skyblue" align = "center" >
 	<tr align = "center" bgcolor="skybule">
 	<td style="font-size:30">학년</td>
@@ -12,6 +12,5 @@
 	<td style="font-size:30">전화번호</td>
 	</tr>
 	</table>
-	HTML;
-	echo $xyz;
+
 ?>
