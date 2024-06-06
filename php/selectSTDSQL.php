549c604c36ff873abc518bb8a11f56724746036e
김겨리-SMU <81796346+gyeori0805@users.noreply.github.com>
2024-06-06 23:22:55 +0900

    Update selectSTDSQL.php

--- php/selectSTDSQL.php
+++ php/selectSTDSQL.php
@@ -24,6 +24,27 @@
 	</html>
  	';
 
+	// MySQL 책 검색 실행 및 결과 출력
+	$query = "select * from student where name like '%".$message."%';";
+	$resultSet = mysqli_query( $conn, $query );
+	while( $result = mysqli_fetch_array( $resultSet ) )
+	{
+		echo '
+		<table border="1" bordercolor="skyblue" align = "center" >
+		<tr align = "center">
+		<td style="font-size:25">.$result['grade']</td>
+		<td style="font-size:25">.$result['studentid']</td>
+		<td style="font-size:25">.$result['name']</td>
+		<td style="font-size:25">.$result['birth']</td>
+		<td style="font-size:25">.$result['gender']</td>
+		<td style="font-size:25">.$result['studentrecord']</td>
+		<td style="font-size:25">.$result['phonenumber']</td>
+		</tr>
+		</table>
+		</html>
+  		';
+	}
+
 	// MySQL 드라이버 연결 해제
 	mysqli_free_result( $resultSet );
 	mysqli_close( $conn );
