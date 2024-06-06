875b1f50b846795f23bd3c30a9d9675166fd5cb5
김겨리-SMU <81796346+gyeori0805@users.noreply.github.com>
2024-06-06 22:59:33 +0900

    Update selectSTDSQL.php

--- php/selectSTDSQL.php
+++ php/selectSTDSQL.php
@@ -1,15 +1,10 @@
 <?php header('Content-Type: text/html; charset=UTF-8'); ?>
 <?php 
 	echo "<html>";
-	echo "<table border="1" bordercolor="skyblue" align = "center" >";
-	echo "<tr align = "center" bgcolor="skybule">";
-	echo "<td style="font-size:30">학년</td>";
-	echo "<td style="font-size:30">학번</td>";
-	echo "<td style="font-size:30">이름</td>";
-	echo "<td style="font-size:30">생년월일</td>";
-	echo "<td style="font-size:30">성별</td>";
-	echo "<td style="font-size:30">학적</td>";
-	echo "<td style="font-size:30">전화번호</td>";
+	echo "<table>";
+	echo "<tr>";
+	echo "<td>학년</td>";
+	echo "<td>학번</td>";
 	echo "</tr>";
 	echo "</table>";
 	echo "</html>";
