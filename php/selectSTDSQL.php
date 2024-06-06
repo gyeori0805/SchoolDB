306c687133a9dfbbd96f519673b7fff25b67392c
김겨리-SMU <81796346+gyeori0805@users.noreply.github.com>
2024-06-06 23:34:26 +0900

    Update selectSTDSQL.php

--- php/selectSTDSQL.php
+++ php/selectSTDSQL.php
@@ -20,8 +20,6 @@
 	<td style="font-size:30">학적</td>
 	<td style="font-size:30">전화번호</td>
 	</tr>
-	</table>
-	</html>
  	';
 
 	// MySQL 책 검색 실행 및 결과 출력
@@ -30,7 +28,6 @@
 	while( $result = mysqli_fetch_array( $resultSet ) )
 	{
 		echo '
-		<table border="1" bordercolor="skyblue" align = "center" >
 		<tr align = "center">
 		<td style="font-size:25">1</td>
 		<td style="font-size:25">2</td>
