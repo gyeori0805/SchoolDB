98f29a7a2d4e2bc8672b351598cfefacdb23f6a0
김겨리-SMU <81796346+gyeori0805@users.noreply.github.com>
2024-06-06 23:11:48 +0900

    Update selectSTDSQL.php

--- php/selectSTDSQL.php
+++ php/selectSTDSQL.php
@@ -2,7 +2,7 @@
 <?php 
 	echo '
  	<html>
-	<table class="html-table" border="1" bordercolor="skyblue" align = "center">
+	<table border="1" bordercolor="skyblue" align = "center">
 	<tr align = "center" bgcolor="skybule">
 	<td>학년</td>
 	<td>학번</td>
