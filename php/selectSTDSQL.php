292755d4da5f00d5f310576a90719fb6bbf6fc7b
김겨리-SMU <81796346+gyeori0805@users.noreply.github.com>
2024-06-06 22:19:40 +0900

    Update selectSTDSQL.php

--- php/selectSTDSQL.php
+++ php/selectSTDSQL.php
@@ -1,4 +1,5 @@
 <?php header('Content-Type: text/html; charset=UTF-8'); ?>
+<html>
 <?php 
 	// 이전 페이지에서 전달 받은 메시지 확인
 	$message =  $_POST['message'];
@@ -7,23 +8,19 @@
 	// MySQL 드라이버 연결 
 	include("./SQLconstants.php");
 	$conn = mysqli_connect($mySQL_host,$mySQL_id,$mySQL_password,$mySQL_database) or die ("Can't access DB");
-
-	echo "
-	<html>
-		<table border="1" bordercolor="skyblue" align = "center" >
-			<tr align = "center" bgcolor="skybule">
-				<td style="font-size:30">학년</td>
-				<td style="font-size:30">학번</td>
-				<td style="font-size:30">이름</td>
-				<td style="font-size:30">생년월일</td>
-				<td style="font-size:30">성별</td>
-				<td style="font-size:30">학적</td>
-				<td style="font-size:30">전화번호</td>
-			</tr>
-		</table>
-	</html>
-	";
-
+?>
+	<table border="1" bordercolor="skyblue" align = "center" >
+		<tr align = "center" bgcolor="skybule">
+			<td style="font-size:30">학년</td>
+			<td style="font-size:30">학번</td>
+			<td style="font-size:30">이름</td>
+			<td style="font-size:30">생년월일</td>
+			<td style="font-size:30">성별</td>
+			<td style="font-size:30">학적</td>
+			<td style="font-size:30">전화번호</td>
+		</tr>
+	</table>
+<?php 
 	// MySQL 책 검색 실행 및 결과 출력
 	$query = "select * from student where name like '%".$message."%';";
 	$resultSet = mysqli_query( $conn, $query );
@@ -36,6 +33,7 @@
 	mysqli_free_result( $resultSet );
 	mysqli_close( $conn );
 ?>
+</html>
 
 <?php 
 	// 로그 데이터 추출
